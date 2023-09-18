5d04bebb00dba1773ac9604bdac3290199feee4a
harsh-the-innovator <harshgandhi043@gmail.com>
2023-09-18 21:10:38 +0530

    Add customer form html page and linked with controller

--- src/main/java/com/example/springdemo/mvc/controller/CustomerController.java
+++ src/main/java/com/example/springdemo/mvc/controller/CustomerController.java
@@ -1,4 +1,19 @@
 package com.example.springdemo.mvc.controller;
 
+import com.example.springdemo.mvc.Customer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CustomerController {
+
+    // model allows us to share the information between controllers and view pages
+    @GetMapping("/")
+    public String showForm(Model model) {
+
+        model.addAttribute("customer", new Customer());
+
+        return "customer-form"; // will return customer-form.html page
+    }
 }
