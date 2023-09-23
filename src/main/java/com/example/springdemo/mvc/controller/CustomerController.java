80d2814dad34f347e229f6a12ec1de957fb8829e
harsh-the-innovator <harshgandhi043@gmail.com>
2023-09-23 20:07:28 +0530

    Added processForm api:
     - Created new api route for processing form after submission
     - Handled errors for the form fields.
     - Created customer-confirmation.html page for successfull form
       submission.

--- src/main/java/com/example/springdemo/mvc/controller/CustomerController.java
+++ src/main/java/com/example/springdemo/mvc/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.example.springdemo.mvc.controller;
 
 import com.example.springdemo.mvc.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -15,5 +19,16 @@ public class CustomerController {
         model.addAttribute("customer", new Customer());
 
         return "customer-form"; // will return customer-form.html page
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer,
+                              BindingResult theBindingResult
+    ){
+        if(theBindingResult.hasErrors()){
+            return "customer-form";
+        }
+
+        return "customer-confirmation";
     }
 }
