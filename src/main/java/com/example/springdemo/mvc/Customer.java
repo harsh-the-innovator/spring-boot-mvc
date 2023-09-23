1ccd6abb738dc6047126a46f67944220976ebb8b
harsh-the-innovator <harshgandhi043@gmail.com>
2023-09-23 22:33:14 +0530

    Added new form field:
     - Added new field for postalCode.
     - Added regex pattern validation
     - Also removed error for freePasses field, if freePasses are passed
       empty in the form field

--- src/main/java/com/example/springdemo/mvc/Customer.java
+++ src/main/java/com/example/springdemo/mvc/Customer.java
@@ -1,10 +1,7 @@
 package com.example.springdemo.mvc;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -14,9 +11,12 @@ public class Customer {
     @Size(min = 1,message = "is required")
     private String lastName = "";
 
+    @NotNull(message = "Cannot be empty")
     @Min(value = 0,message = "Must be greater than or equal to 0")
     @Max(value = 10, message = "Must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
+
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -34,11 +34,20 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
+    }
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}",message = "Only 4 chars/digits")
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
