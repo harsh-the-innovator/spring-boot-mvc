1657c2e7e5e20ac746757e9793f09d85f8d11d1f
harsh-the-innovator <harshgandhi043@gmail.com>
2023-09-23 20:21:36 +0530

    Added initBinder for preprocessing all request data  - Here we are using for trimming leading and trailing white spaces in form data

--- src/main/java/com/example/springdemo/mvc/controller/CustomerController.java
+++ src/main/java/com/example/springdemo/mvc/controller/CustomerController.java
@@ -2,15 +2,27 @@ package com.example.springdemo.mvc.controller;
 
 import com.example.springdemo.mvc.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initbinder to preprocess all requests
+    // Here for trimming the trailing and leading white space in form input data
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     // model allows us to share the information between controllers and view pages
     @GetMapping("/")
